82d481743fde72070b1450070c3eb981ff79338d
Quan-Sheng Zeng <zengqs@mail.nankai.edu.cn>
2023-04-03 13:11:58 +0800

    update configs

---
+++ exp/deeplabv3/deeplabv3_r50-d8_512x512_80k_dm3-0.5-dcrf-2.0-0.05-0.95_base_format-vocaug-dcrf.py
@@ -0,0 +1,21 @@
+_base_ = ['./deeplabv3_r50-d8_512x512_80k_dm3-0.5-dcrf-2.0-0.05-0.95_base.py']
+
+model = dict(
+    post_cfg=dict(
+        method='dcrf',
+        kwargs=dict(iter_max=10,
+                    pos_xy_std=1,
+                    pos_w=3,
+                    bi_xy_std=67,
+                    bi_rgb_std=3,
+                    bi_w=4, )
+    )
+)
+
+data = dict(
+    test=dict(
+        type='PascalVOCDataset2',
+        ann_dir='SegmentationClassAUG',
+        split='ImageSets/Segmentation/train_aug.txt'
+    )
+)
